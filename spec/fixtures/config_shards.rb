21492a60e761e3931ad2cfd4401beaca053cba83
Michael L. Artz <michael.artz@redjack.com>
2014-03-15 15:36:01 -0400

    Updated spec test for new shard DSL

--- spec/fixtures/config_shards.rb
+++ spec/fixtures/config_shards.rb
@@ -4,18 +4,18 @@ RFlow::Configuration::RubyDSL.configure do |config|
   config.setting('rflow.application_name', 'shardapp')
 
   # Instantiate components
-  config.shard(:process, 1) do |shard|
+  config.shard 's1', :process => 1 do |shard|
     shard.component 'generate_ints1', 'RFlow::Components::GenerateIntegerSequence', 'start' => 0, 'finish' => 10, 'step' => 3
   end
 
-  config.shard(:process, 2) do |shard|
+  config.shard 's2', :type => :process, :count => 2 do |shard|
     shard.component 'generate_ints2', 'RFlow::Components::GenerateIntegerSequence', 'start' => 20, 'finish' => 30
   end
 
   config.component 'filter', 'RFlow::Components::RubyProcFilter', 'filter_proc_string' => 'lambda {|message| true}'
   config.component 'replicate', 'RFlow::Components::Replicate'
 
-  config.shard(:process, 2) do |shard|
+  config.shard 's3', :process => 2 do |shard|
     shard.component 'output1', 'RFlow::Components::FileOutput', 'output_file_path' => 'out1'
     shard.component 'output2', 'RFlow::Components::FileOutput', 'output_file_path' => 'out2'
   end
