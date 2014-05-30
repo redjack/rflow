a9697a75a7a23af5e29350fbc16daa1f93006a29
John Stoneham <jstoneham@texeltek.com>
2014-05-29 22:56:32 -0400

    Refactor rflow/logger.rb.

--- lib/rflow/logger.rb
+++ lib/rflow/logger.rb
@@ -9,34 +9,24 @@ class RFlow
     DATE_METHOD = 'xmlschema(6)'
     LOG_PATTERN_FORMATTER = PatternFormatter.new :pattern => LOG_PATTERN_FORMAT, :date_method => DATE_METHOD
 
-    attr_accessor :config, :internal_logger
-    attr_accessor :log_file_path, :log_level
+    private
+    attr_accessor :internal_logger
+    attr_accessor :log_file_path, :log_level, :log_name
+
+    public
 
     # Delegate log methods to internal logger
     def_delegators :@internal_logger, :fatal, :error, :warn, :info, :debug
 
-    def initialize(config, include_stdout = nil)
+    def initialize(config, include_stdout = false)
       @log_file_path = config['rflow.log_file_path']
       @log_level = config['rflow.log_level']
+      @log_name = (config['rflow.application_name'] || File.basename(log_file_path))
 
-      log_name = (config['rflow.application_name'] || File.basename(log_file_path))
-      @internal_logger = Log4r::Logger.new(log_name)
-
-      internal_logger.level = LNAMES.index log_level
-      internal_logger.trace = true
-
-      begin
-        internal_logger.add FileOutputter.new('rflow.log_file', :filename => log_file_path, :formatter => LOG_PATTERN_FORMATTER)
-      rescue Exception => e
-        raise ArgumentError, "Log file '#{File.expand_path log_file_path}' problem: #{e.message}\b#{e.backtrace.join("\n")}"
-      end
-
-      if include_stdout
-        internal_logger.add StdoutOutputter.new('rflow_stdout', :formatter => LOG_PATTERN_FORMATTER)
-      end
-
-      Log4r::NDC.clear
-      Log4r::NDC.push(log_name)
+      establish_internal_logger
+      hook_up_logfile
+      hook_up_stdout if include_stdout
+      register_logging_context
 
       internal_logger
     end
@@ -58,6 +48,31 @@ class RFlow
 
       internal_logger.warn "Changing log level from #{original_log_level} to #{new_log_level}"
       internal_logger.level = LNAMES.index new_log_level
+    end
+
+    private
+    def establish_internal_logger
+      @internal_logger = Log4r::Logger.new(log_name).tap do |logger|
+        logger.level = LNAMES.index log_level
+        logger.trace = true
+      end
+    end
+
+    def hook_up_logfile
+      begin
+        internal_logger.add FileOutputter.new('rflow.log_file', :filename => log_file_path, :formatter => LOG_PATTERN_FORMATTER)
+      rescue Exception => e
+        raise ArgumentError, "Log file '#{File.expand_path log_file_path}' problem: #{e.message}\b#{e.backtrace.join("\n")}"
+      end
+    end
+
+    def hook_up_stdout
+      internal_logger.add StdoutOutputter.new('rflow_stdout', :formatter => LOG_PATTERN_FORMATTER)
+    end
+
+    def register_logging_context
+      Log4r::NDC.clear
+      Log4r::NDC.push(log_name)
     end
   end
 end
