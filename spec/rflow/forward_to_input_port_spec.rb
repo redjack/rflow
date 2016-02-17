b018a37d1bd0d0d3e4543560a3774833858475c3
John Stoneham <lyric@lyrically.net>
2016-02-17 12:49:10 -0500

    #81 Improve unit tests.

--- spec/rflow/forward_to_input_port_spec.rb
+++ spec/rflow/forward_to_input_port_spec.rb
@@ -7,34 +7,49 @@ class RFlow
       Configuration.migrate_database
     end
 
-    let(:filtered_messages) { [] }
+    let(:accepted_messages) { [] }
     let(:dropped_messages) { [] }
 
     let(:generator) do
       RFlow::Components::GenerateIntegerSequence.new.tap do |c|
         c.configure!({})
-        c.out.direct_connect ruby_proc_filter.in
       end
     end
 
-    let(:ruby_proc_filter) do
+    let(:accept_evens) do
       RFlow::Components::RubyProcFilter.new.tap do |c|
         c.configure! 'filter_proc_string' => 'message.data.data_object % 2 == 0'
       end
     end
 
     it 'should forward generated integers to be filtered by the proc filter' do
-      ruby_proc_filter.filtered.collect_messages(nil, filtered_messages) do
-        ruby_proc_filter.dropped.collect_messages(nil, dropped_messages) do
+      generator.out.direct_connect accept_evens.in
+
+      accept_evens.filtered.collect_messages(nil, accepted_messages) do
+        accept_evens.dropped.collect_messages(nil, dropped_messages) do
           5.times { generator.generate }
         end
       end
 
-      expect(filtered_messages).to have(3).messages
-      expect(filtered_messages.map(&:data).map(&:data_object)).to eq([0, 2, 4])
+      expect(accepted_messages).to have(3).messages
+      expect(accepted_messages.map(&:data).map(&:data_object)).to eq([0, 2, 4])
+      expect(dropped_messages).to have(2).messages
+      expect(dropped_messages.map(&:data).map(&:data_object)).to eq([1, 3])
+    end
+
+    it 'should forward integers from the union of subports' do
+      generator.even_odd_out.direct_connect accept_evens.in
+
+      accept_evens.filtered.collect_messages(nil, accepted_messages) do
+        accept_evens.dropped.collect_messages(nil, dropped_messages) do
+          5.times { generator.generate }
+        end
+      end
+
+      expect(accepted_messages).to have(3).messages
+      expect(accepted_messages.map(&:data).map(&:data_object)).to eq([0, 2, 4])
       expect(dropped_messages).to have(2).messages
       expect(dropped_messages.map(&:data).map(&:data_object)).to eq([1, 3])
     end
   end
 end
-
